ba080b12acae5cb4eba7902fd617a50a47657a1f
Zivile <zivileriaukaite@gmail.com>
2020-07-02 19:01:56 +0200

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div>
+      <footer>
+        <p>Copyright ⓒ {currentYear}</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
